76dca8bebc3553487a922fabddfdda256b3a4aa6
Pavan-KumarCH <pavun9848@gamil.com>
2024-08-13 19:34:25 +0530

    Updated

--- SRC/Components/Data_Ingestion.py
+++
@@ -1,9 +0,0 @@
-import os
-import logger
-from roboflow import Roboflow
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
-
-API_KEY = os.environ['API_KEY']
-rf = Roboflow(api_key = API_KEY)
